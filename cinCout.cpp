1075a75c1eddf2fbbf30f55d38d1cd444bdfb27c
唐怀金 <961881550@qq.com>
2019-04-22 20:45:03 +0800

    Add files via upload

---
+++ cinCout.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=0;
+    int b=0;
+    cout<<"put in number"<<endl;
+    cin>>a>>b;
+    cout<<"you give a+b:"<<a+b<<endl;
+    printf("hello,byebye\n");
+return 0;
+}
